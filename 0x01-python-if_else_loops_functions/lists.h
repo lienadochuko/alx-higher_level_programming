cac2e72647c455b10d51de94593cdbb1a0f0d5ed
lienadochuko <erhisowomedanielking@gmail.com>
2022-11-29 20:21:54 +0000

    if and else

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - stingly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Alx project
+ */
