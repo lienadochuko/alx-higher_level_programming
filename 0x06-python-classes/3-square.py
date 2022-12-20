f6f1304f8f1acb86f300b1aaf2d31dc2eaab7a3b
lienadochuko <erhisowomedanielking@gmail.com>
2022-12-20 23:18:18 +0100

    instance attribute

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,24 @@
 #!/user/bin/python3
+"""Define a class square."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        x=self.__size * self.__size
+        return (x)
+        
