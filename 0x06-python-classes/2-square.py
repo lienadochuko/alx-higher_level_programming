06fdb3fb002d4cbd1638be11a26e76cf85e5dcce
lienadochuko <erhisowomedanielking@gmail.com>
2022-12-20 23:00:31 +0100

    size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Define a class square."""
+
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): size of the new square.
+        """
+        self.__size = size
